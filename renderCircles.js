788bf7178e1041e380ac11b2e16faa45b469098a
Terrence Eve <tijayeve@gmail.com>
2019-08-06 22:15:52 -0500

    Completed renderRectangle, have not gotten circles to render yet

--- renderCircles.js
+++ renderCircles.js
@@ -1,15 +1,45 @@
 
-function renderCircles(circles) {
+function renderCircles(circles) {                      //function that renders circles
     // HINT: You probably need to write a for loop!
-    //       Or, if you're feeling fancy, use .map() 
+    //       Or, if you're feeling fancy, use .map()
+
+    let circleData = []
+    for (let index = 0; index < circles.length; index++) { // iterate through the length of the circles function. The for loop can't access circlesAbstraction, but can access circles() so use circles.length
+        let radius = circles[index].radius
+        let color = circles[index].color
+
+        let div = `<div style = "border-radius: ${radius}; color: ${color}"></div>`
+
+        circleData.push(div)                  
+    }
+    console.log(circleData)
+
+
+    //create a new array with the information in circles() then use the new array to input information. You can do this using the map function!
+
+    // function createCircleHTML (obj) {  //this function is supposed to create a div for html
+    //     let div = `<div style = "border-radius: ${circles.radius} color: ${circles.color}"</div>`
+    //     return div
+    // }
+
+    // //map function that uses create createCircleHTML
+
+    // let newArr = circles.map(createCircleHTML)
+
+    // console.log(circles.map())
+
+
+    let circleDivs = circleData.join()
+    console.log(circleDivs)
+
     return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(circles)}</code>
+           <div>${circleDivs}</div>
         </div>
     `
 }
 
-function circles() {
+function circles() {                                    //this function has contains an array of objects with information that will be given to renderCircles
     var content = document.getElementById('content');
 
     var circlesAbstraction = [
@@ -32,5 +62,4 @@ function circles() {
     ];
 
     content.innerHTML = renderCircles(circlesAbstraction);
-
 }
--- renderRectangle.js
+++ renderRectangle.js
@@ -1,21 +1,26 @@
 
 function renderRectangle(rectangle) {
+    console.log(rectangle.width)                   //renderRectangle takes rectangle as a parameter. Rectangle is a function.
     return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(rectangle)}</code>
+            <div style = "
+                width: ${rectangle.width}px;
+                height: ${rectangle.height}px;
+                background-color: ${rectangle.color}">
+             </div>
         </div>
-    `
+    `                                                   //this function returns HTML code that is then given to content
 }
 
 function rectangle() {
-    var content = document.getElementById('content');
+    var content = document.getElementById('content');   //defines where to find the content in the DOM
 
-    var rectangleAbstraction = {
+    var rectangleAbstraction = {                        //this is the abstracted rectangle information: w,h,& color
         width: 100,
         height: 200,
         color: "#FF00FF"
     }
 
-    content.innerHTML = renderRectangle(rectangleAbstraction);
+    content.innerHTML = renderRectangle(rectangleAbstraction);    //this changes HTML of the content element to render the rectangle
 
 }
